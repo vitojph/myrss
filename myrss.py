c0f43d3f0e07b58dc9902e66001d63956efb63fb
Victor Peinado <vitojph@gmail.com>
2014-08-10 18:09:38 +0200

    Added: NER and dbpedia URIs thru AlchemyAPI

--- myrss.py
+++ myrss.py
@@ -4,20 +4,28 @@
 from flask import Flask
 from flask import render_template
 from pattern.web import Newsfeed, plaintext
-#from alchemyapi import AlchemyAPI
+from alchemyapi import AlchemyAPI
 
 app = Flask(__name__)
 reader = Newsfeed()
-#alchemyapi = AlchemyAPI()
+alchemyapi = AlchemyAPI()
 
 LIFEHACKER = "http://feeds.gawker.com/lifehacker/vip"
 
 items = []
 for result in reader.search(LIFEHACKER)[:10]:
-  clean_text = plaintext(result.text, keep={'h1':[], 'h2':[], 'strong':[], 'a':['href']})
-  items.append(dict(title=result.title, url=result.url, text=clean_text))
+  clean_text = plaintext(result.text)
+  response = alchemyapi.entities("text", result.text)
 
-print type(items), len(items)
+  entities = []
+  for entity in response["entities"]:
+    if entity.has_key("disambiguated"):
+      dbpedia_uri = entity["disambiguated"]["dbpedia"]
+    else:
+      dbpedia_uri = None
+    entities.append((entity["text"], dbpedia_uri))
+
+  items.append(dict(title=result.title, url=result.url, text=clean_text, entities=entities))
 
 @app.route('/')
 def index():
