2ebbe0cbc48cc14d35d181da91ce963f183eb814
Victor Peinado <vitojph@gmail.com>
2014-08-11 16:41:10 +0200

    added other RSS feeds and CSS style

--- myrss.py
+++ myrss.py
@@ -10,22 +10,33 @@ app = Flask(__name__)
 reader = Newsfeed()
 alchemyapi = AlchemyAPI()
 
-LIFEHACKER = "http://feeds.gawker.com/lifehacker/vip"
+RSS_LIST = [
+  (u"Lifehacker", "http://feeds.gawker.com/lifehacker/vip"),
+  (u"The Verge", "http://www.theverge.com/rss/index.xml"),
+  (u"Naukas", "http://naukas.com/feed/"),
+  (u"Zen Habits", "http://feeds.feedburner.com/zenhabits?format=xml"),
+  (u"Yuri", "http://www.lapizarradeyuri.com/feed/"),
+  (u"Menéame", "http://www.meneame.net/rss")
+]
 
 items = []
-for result in reader.search(LIFEHACKER)[:10]:
-  clean_text = plaintext(result.text)
-  response = alchemyapi.entities("text", result.text)
 
-  entities = []
-  for entity in response["entities"]:
-    if entity.has_key("disambiguated"):
-      dbpedia_uri = entity["disambiguated"]["dbpedia"]
-    else:
-      dbpedia_uri = None
-    entities.append((entity["text"], dbpedia_uri))
+for feed in RSS_LIST:
+  feedlist = []
+  for result in reader.search(feed[1])[:10]:
+    clean_text = plaintext(result.text)
+    response = alchemyapi.entities("text", result.text)
 
-  items.append(dict(title=result.title, url=result.url, text=clean_text, entities=entities))
+    entities = []
+    for entity in response["entities"]:
+      if entity.has_key("disambiguated"):
+        dbpedia_uri = entity["disambiguated"]["dbpedia"]
+      else:
+        dbpedia_uri = None
+      entities.append((entity["text"], dbpedia_uri))
+
+    feedlist.append(dict(title=result.title, url=result.url, text=clean_text, entities=entities))
+  items.append(dict(site=feed[0], feedlist=feedlist))
 
 @app.route('/')
 def index():
